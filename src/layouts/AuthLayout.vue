58d304e3bd945d200fc11eebbe23a06ca4a0fad9
alexdeploy <me@alexrueda.dev>
2023-07-03 20:26:18 +0200

    Adding Layout and app config

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="auth-layout">
+      <header>
+        <nav>
+            <p>Layout Header</p>
+        </nav>
+      </header>
+      <main>
+        <div class="wrapper">
+          <RouterView />
+        </div>
+      </main>
+      <footer>
+        <p>Layout Footer</p>
+      </footer>
+    </div>
+  </template>
+  
+  <style scoped>
+  .auth-layout {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  
+  .auth-layout main {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 2em;
+  }
+  
+  .wrapper {
+    max-width: 400px;
+    width: 400px;
+  }
+  
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -4,34 +4,44 @@ import SignInView from '../views/auth/SignIn.vue'
 import SignUpView from '../views/auth/SignUp.vue'
 import ForgotPasswordView from '../views/auth/ForgotPassword.vue'
 import ResetPasswordView from '../views/auth/ResetPassword.vue'
+import AuthLayout from '../layouts/AuthLayout.vue'
+import config from '../../app.config.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: config.routes.home.url,
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      /* redirect: config.routes.auth.sign_in.url */
     },
     {
-      path: '/sign-in',
-      name: 'signIn',
-      component: SignInView
-    },
-    {
-      path: '/sign-up',
-      name: 'signUp',
-      component: SignUpView
-    },
-    {
-      path: '/forgot-password',
-      name: 'forgotPassword',
-      component: ForgotPasswordView
-    },,
-    {
-      path: '/reset-password',
-      name: 'resetPassword',
-      component: ResetPasswordView
+      path: config.routes.auth.base,
+      component: AuthLayout,
+      redirect: config.routes.auth.sign_in.url,
+      children: [
+        {
+          path: config.routes.auth.sign_in.url,
+          name: 'SignIn',
+          component: SignInView
+        },
+        {
+          path: config.routes.auth.sign_up.url,
+          name: 'SignUp',
+          component: SignUpView
+        },
+        {
+          path: config.routes.auth.forgot_password.url,
+          name: 'ForgotPassword',
+          component: ForgotPasswordView
+        },
+        {
+          path: config.routes.auth.reset_password.url,
+          name: 'ResetPassword',
+          component: ResetPasswordView
+        }
+      ]
     }
   ]
 })
